e0d6bdbf87e4b37fb39ae2f70b49816b40c31810
Pulkit Mishra <pulkitmishra007@gmail.com>
2019-03-30 09:35:36 +0530

    Adds code for frontend

---
+++ server/app/views.py
@@ -0,0 +1,10 @@
+from app import app
+import json
+from flask import render_template, jsonify, url_for, request, session, redirect
+# values is a list of lists
+time = ["10","20","30","40","50","60","70","80"]
+values = [10,9,8,7,6,4,7,8]
+
+@app.route('/')
+def index():
+    return render_template('index.html', labels=time, values=values)
